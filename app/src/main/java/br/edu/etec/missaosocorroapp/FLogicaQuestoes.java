07d9dd79977e23deec3f244dc60c9a668691bb77
leandro-etec-2025 <leandrocesardacruz.cps.2025@gmail.com>
2025-11-28 14:03:29 -0300

    feat: adicionadas respostas e soma das notas

--- app/src/main/java/br/edu/etec/missaosocorroapp/FLogicaQuestoes.java
+++ app/src/main/java/br/edu/etec/missaosocorroapp/FLogicaQuestoes.java
@@ -3,6 +3,8 @@ package br.edu.etec.missaosocorroapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,12 +14,31 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class FLogicaQuestoes extends AppCompatActivity {
 
-    Button botao_voltar;
+    Button botao_voltar, btnA, btnB, btnC;
+    TextView textoPergunta;
+    
+    int indice = 0;
+
+    int acertos = 0;
+
+    String[] perguntas = {
+            "1) Qual é a resposta correta da primeira questão?",
+            "2) Segunda pergunta: qual alternativa está correta?",
+            "3) Última questão, escolha a certa!"
+    };
+
+    int[] respostas = {
+            1,
+            2,
+            2
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.f_tela_questoes);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -25,15 +46,46 @@ public class FLogicaQuestoes extends AppCompatActivity {
         });
 
         botao_voltar = findViewById(R.id.btn_voltar);
+        textoPergunta = findViewById(R.id.txv_descricao_niveis);
+        btnA = findViewById(R.id.btn_alternativa_a);
+        btnB = findViewById(R.id.btn_alternativa_b);
+        btnC = findViewById(R.id.btn_alternativa_c);
+
+        atualizarPergunta();
 
         botao_voltar.setOnClickListener(v -> {
-            Intent voltar = new Intent(
-                    FLogicaQuestoes.this,
-                    BLogicaIdade.class
-            );
+            Intent voltar = new Intent(FLogicaQuestoes.this, BLogicaIdade.class);
             startActivity(voltar);
             finish();
         });
 
+        btnA.setOnClickListener(v -> verificarResposta(0));
+        btnB.setOnClickListener(v -> verificarResposta(1));
+        btnC.setOnClickListener(v -> verificarResposta(2));
+    }
+
+    private void atualizarPergunta() {
+        textoPergunta.setText(perguntas[indice]);
+    }
+
+    private void verificarResposta(int respostaEscolhida) {
+
+        if (respostaEscolhida == respostas[indice]) {
+            acertos++;
+        }
+
+        indice++;
+
+        if (indice < perguntas.length) {
+            atualizarPergunta();
+        } else {
+            Toast.makeText(
+                    this,
+                    "Você acertou " + acertos + " de " + perguntas.length + " perguntas!",
+                    Toast.LENGTH_LONG
+            ).show();
+
+            finish();
+        }
     }
 }
